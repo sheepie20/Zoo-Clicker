0960f58de364af0da7f895bfe4a17c60ffb0d1a7
sheepie20 <86570972+sheepie20@users.noreply.github.com>
2025-02-17 15:04:33 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,3 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'Welcome to Zoo Clicker!'
-#weird
-
-print('Good day')
-dog = int(input())
-if dog == (14):
-    print('14 dogs!')
-else:
-    print('f')
-
-
